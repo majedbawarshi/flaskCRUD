b34733357d82b4e22520ea9bebc77b5f33a9b870
Majed Bawarshi <b.majd322@gmail.com>
2021-08-21 13:36:00 +0000

    sending args to view from routes

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_title='flask doc')
 
 if __name__ == "__main__":
     app.run(debug=True)
