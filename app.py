9a0b92f75c529e7edcb9030a5f051e8535312a93
Majed Bawarshi <b.majd322@gmail.com>
2021-08-21 12:21:10 +0000

    you may enter html tags inside the returned text.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'welcome to my world using flask framework'
+    return 'welcome to <h1>My World</h1> using flask framework'
 
 if __name__ == "__main__":
     app.run(debug=True)
