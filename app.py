c6053393135084c35d74ebb4aad1a6477aea69b3
Majed Bawarshi <b.majd322@gmail.com>
2021-08-21 15:29:26 +0000

    adding bootstrap cdn, navbar, about page

--- app.py
+++ app.py
@@ -5,7 +5,11 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', current_title='flask doc')
+    return render_template('index.html', title='Home')
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='about')
 
 if __name__ == "__main__":
     app.run(debug=True)
