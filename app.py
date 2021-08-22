7384b627828989e225a4504c95d91309293146cf
Majed Bawarshi <b.majd322@gmail.com>
2021-08-22 13:39:00 +0000

    refactor the app bycompingin the routes under one file.

--- app.py
+++ app.py
@@ -1,15 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html', title='Home')
-
-@app.route('/about')
-def about():
-    return render_template('about.html', title='about')
+from routes import *
 
 if __name__ == "__main__":
     app.run(debug=True)
