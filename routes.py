a991454540576923f3c5c68d85cef8382919c9c6
Majed Bawarshi <b.majd322@gmail.com>
2021-08-22 14:30:58 +0000

    form post method simple example

--- routes.py
+++ routes.py
@@ -8,7 +8,12 @@ import forms
 def index():
     return render_template('index.html', title='Home')
 
-@app.route('/about')
+# the routes expect the request method to be get by default
+@app.route('/about', methods=['GET', 'POST'])
 def about():
     form = forms.AddTaskForm()
+    # receving the information sended by the form
+    if form.validate_on_submit():
+        # this message will be printed on the cmd panel when a form is submitted.
+        print('Information has been submitted.\nThe data are', form.title.data)
     return render_template('about.html', title='about', form=form)
